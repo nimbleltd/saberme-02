e4765498cd4c0ecf5f60e46a5e8f0998daedee9a
Paul Campbell <nimbleltd@gmail.com>
2015-10-29 10:00:03 -0500

    updated routes and navbar

---
+++ client/templates/main.js
@@ -0,0 +1,46 @@
+ // Tasks.remove(this._id);
+
+ //  });
+ 
+  // Accounts.ui.config({
+  //   passwordSignupFields: "USERNAME_ONLY"
+  // });
+
+// list all users
+// Template.content.users = function () {
+//   return Meteor.users.find();
+// };
+
+// AccountsTemplates.configure({
+//     texts: {
+//         navSignIn: "signIn",
+//         navSignOut: "signOut",
+//         optionalField: "optional",
+//         pwdLink_pre: "",
+//         pwdLink_link: "forgotPassword",
+//         pwdLink_suff: "",
+//         resendVerificationEmailLink_pre: "Verification email lost?",
+//         resendVerificationEmailLink_link: "Send again",
+//         resendVerificationEmailLink_suff: "",
+//         sep: "OR",
+//         signInLink_pre: "ifYouAlreadyHaveAnAccount",
+//         signInLink_link: "signin",
+//         signInLink_suff: "",
+//         signUpLink_pre: "dontHaveAnAccount",
+//         signUpLink_link: "signUp",
+//         signUpLink_suff: "",
+//         socialAdd: "add",
+//         socialConfigure: "configure",
+//         socialIcons: {
+//             "meteor-developer": "fa fa-rocket",
+//         },
+//         socialRemove: "remove",
+//         socialSignIn: "signIn",
+//         socialSignUp: "signUp",
+//         socialWith: "with",
+//         termsPreamble: "clickAgree",
+//         termsPrivacy: "privacyPolicy",
+//         termsAnd: "and",
+//         termsTerms: "terms",
+//     }
+// };
